9dc3af02c3bd8941e690607d1fc1200ca07843ec
aktamovasad1-wq <aktamovasad1@gmail.com>
2026-08-14 17:23:38 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.MODEX_CONFIG = {
   SUPABASE_URL: "https://rkfohcbgqfjgwbybbnnv.supabase.co",
-  SUPABASE_KEY: "sb_publishable_16dZVH9Coki-gAyf3T85Vw_yJlQ_EYj"
+  SUPABASE_KEY: "BU_YERGA_SUPABASE_PUBLISHABLE_KEY"
 };
